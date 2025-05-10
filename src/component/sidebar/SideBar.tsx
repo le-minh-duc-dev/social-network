3b57f293df1419b98a2c8cea03b12150a69b8e44
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-10 21:56:11 +0700

    fix sidebar z index covered by post list

--- src/component/sidebar/SideBar.tsx
+++ src/component/sidebar/SideBar.tsx
@@ -11,7 +11,7 @@ export default async function Sidebar() {
   const session = await auth()
   if(!session?.user) return null
   return (
-    <div className="fixed lg:w-72 2xl:w-[345px] flex flex-col border-r top-0 left-0 h-screen border-r-white/15 pl-6 pr-4 pt-12">
+    <div className="fixed lg:w-72 2xl:w-[345px] flex flex-col border-r top-0 left-0 h-screen border-r-white/15 pl-6 pr-4 pt-12 z-10">
       <h2 className={pacifico.className + ` text-xl`}>Social Network</h2>
       <div className="mt-16 flex flex-col justify-between max-h-full flex-1 pb-8">
         <LinkList />
