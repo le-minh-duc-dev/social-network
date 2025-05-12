def6a4fbf43a96843042f3f7f00719cef8a5a9b0
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-12 21:46:24 +0700

    sidebar to client component

--- src/component/sidebar/SideBar.tsx
+++ src/component/sidebar/SideBar.tsx
@@ -1,15 +1,18 @@
+"use client"
 import React from "react"
 import { Pacifico } from "next/font/google"
 import LinkList from "./LinkList"
 import More from "./More"
-import { auth } from "@/auth/auth"
+import { useAuth } from "@/hooks/useAuth"
+import { usePathname } from "next/navigation"
 const pacifico = Pacifico({
   subsets: ["latin"],
   weight: "400",
 })
-export default async function Sidebar() {
-  const session = await auth()
-  if(!session?.user) return null
+export default function Sidebar() {
+  const { isAuthenticated } = useAuth()
+  const pathname = usePathname()
+  if (!isAuthenticated || pathname.startsWith("/admin")) return null
   return (
     <div className="fixed lg:w-72 2xl:w-[345px] flex flex-col border-r top-0 left-0 h-screen border-r-white/15 pl-6 pr-4 pt-12 z-10">
       <h2 className={pacifico.className + ` text-xl`}>Social Network</h2>
