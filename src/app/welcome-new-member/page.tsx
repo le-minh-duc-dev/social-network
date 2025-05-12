c5371a5ad7a8d651168dbb866fb819792a268fb1
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-12 14:54:11 +0700

    update welcome new member page

---
+++ src/app/welcome-new-member/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+     <div className="flex-1 flex flex-col justify-center items-center">
+      <h1 className=" text-4xl font-bold">Thank you for joining us!</h1>
+      <p className=" text-xl  mt-4">New member account needs administrator&apos;s approval.</p>
+      <p className=" text-xl ">We will email you immediately when the process is done.</p>
+    </div>
+  )
+}
