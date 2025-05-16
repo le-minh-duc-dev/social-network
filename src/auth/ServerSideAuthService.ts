a31e5435d375d61ece65a03e04dc145ee687e2c5
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-16 15:49:14 +0700

    update ServerSideAuthService.ts

--- src/auth/ServerSideAuthService.ts
+++ src/auth/ServerSideAuthService.ts
@@ -1,8 +1,12 @@
+import { AuthUserHelper } from "@/lib/AuthUserHelper"
 import { auth } from "./auth"
 
 export class ServerSideAuthService {
   static async getAuthUser() {
     const authSession = await auth()
-    return authSession?.user
+    if (!authSession) {
+      return null
+    }
+    return AuthUserHelper.getAuthUser(authSession?.user.id)
   }
 }
