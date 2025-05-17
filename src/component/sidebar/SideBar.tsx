db0ffc0da9c1ef124016cd759923ae56106ef8ba
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-17 09:23:24 +0700

    search form in sidebar

--- src/component/sidebar/SideBar.tsx
+++ src/component/sidebar/SideBar.tsx
@@ -5,6 +5,7 @@ import LinkList from "./LinkList"
 import More from "./More"
 import { useAuth } from "@/component/provider/auth/AuthContext"
 import { usePathname } from "next/navigation"
+import Search from "./Search"
 const pacifico = Pacifico({
   subsets: ["latin"],
   weight: "400",
@@ -14,12 +15,13 @@ export default function Sidebar() {
   const pathname = usePathname()
   if ((!isAuthenticated && !isLoading) || pathname.startsWith("/admin")) return null
   return (
-    <div className="fixed lg:w-72 2xl:w-[345px] flex flex-col border-r top-0 left-0 h-screen border-r-white/15 pl-6 pr-4 pt-12 z-10">
+    <div className="fixed lg:w-72 2xl:w-[345px] flex flex-col border-r top-0 left-0 h-screen border-r-white/15 pl-6 pr-4 pt-12 z-10 ">
       <h2 className={pacifico.className + ` text-xl`}>Social Network</h2>
       <div className="mt-16 flex flex-col justify-between max-h-full flex-1 pb-8">
         <LinkList />
         <More />
       </div>
+      <Search/>
     </div>
   )
 }
